65a67d5fa4d03b770ef2e380b602f3426a3ee538
pumuckelo <abdou.seck.business@gmail.com>
2019-07-28 12:20:02 +0200

    forgot methodoverride, added it and initialized

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var expressSanitizer = require("express-sanitizer");
+var methodOverride = require("method-override");
 
 var app = express();
 
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public/"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 //Connect to Database
 mongoose.connect(
